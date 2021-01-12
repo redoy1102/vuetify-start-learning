f1645644c00f2daf3adae085ae98d2c146b98438
Md Fuadul Islam Redoy <mdfuadulislamhridoy52@gmail.com>
2021-01-12 16:49:31 +0600

    Text complete

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="purple darken-4 white--text text-center">This is an about page</h1>
+    <h1 class="display-1">Md Fuadul Islam Redoy</h1>
+    <h1 class="display-2">Md Fuadul Islam Redoy</h1>
+    <h1 class="display-3">Md Fuadul Islam Redoy</h1>
+    <h1 class="display-4">Md Fuadul Islam Redoy</h1>
+    <h1 class="title">Md Fuadul Islam Redoy</h1>
+    <h1 class="caption">Md Fuadul Islam Redoy</h1>
+    <h1 class="subtitle-1">Md Fuadul Islam Redoy</h1>
+    <h1 class="subtitle-2">Md Fuadul Islam Redoy</h1>
+    <h1 class="body-1">Md Fuadul Islam Redoy</h1>
+    <h1 class="subtitle-2">Md Fuadul Islam Redoy</h1>
+
+
   </div>
+
 </template>
